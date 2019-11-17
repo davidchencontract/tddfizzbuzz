eaacb85e9b5c7e2a9a415a09ba782e599dd6af7c
D CHEN <d.chen@recbonus.com>
2019-11-17 23:34:00 +0000

    Refactor code using Tdd

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,22 +2,30 @@ public class FizzBuzz {
     public String numberConverter(final int number) {
         if(number<=0)
             return "Error";
-        if ((number%15)==0)
+        if (isMultipleOfNumber(number, 15))
             return "FizzBuzz";
-        else if (String.valueOf(number).contains("3") && String.valueOf(number).contains("5"))
+        else if (isContainsNumber(number, "3") && isContainsNumber(number, "5"))
             return "FizzBuzz";
-        else if((number%3) == 0 && String.valueOf(number).contains("5"))
+        else if(isMultipleOfNumber(number, 3) && isContainsNumber(number, "5"))
             return "FizzBuzz";
-        else if((number%3) == 0)
+        else if(isMultipleOfNumber(number, 3))
             return "Fizz";
-        else if((number%5)== 0 )
+        else if(isMultipleOfNumber(number, 5))
             return "Buzz";
-        else if(String.valueOf(number).contains("3"))
+        else if(isContainsNumber(number, "3"))
             return "Fizz";
-        else if(String.valueOf(number).contains("5"))
+        else if(isContainsNumber(number, "5"))
             return "Buzz";
         else
             return String.valueOf(number);
+    }
+
+    private boolean isMultipleOfNumber(final int number, final int i) {
+        return (number % i) == 0;
+    }
+
+    private boolean isContainsNumber(final int number, final String s) {
+        return String.valueOf(number).contains(s);
     }
 
     public void printNumber(int printNumber){
